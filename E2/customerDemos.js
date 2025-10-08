589801df4bfa246570399424e49e174db79988f7
Elmerikallio <elmeri.kallio@edu.turkuamk.fi>
2025-10-08 17:13:09 +0300

    eeee

--- E2/customerDemos.js
+++ E2/customerDemos.js
@@ -10,7 +10,20 @@ const port = 3000;
 //   res.send(`Customer ids: ${customerIds.join(", ")}.`);
 // });
 
-app.get("/customers/ids", (req, res) => {
+const logRequestInfo = (req, res, next) => {
+  const { method, url, params, query } = req;
+  console.log(`
+    Request Method: ${method},
+    Request URL: ${url}
+    Request Params: ${JSON.stringify(params)}
+    Request Query: ${JSON.stringify(query)}
+  `);
+  next();
+};
+
+//app.use(logRequestInfo);
+
+app.get("/customers/ids", logRequestInfo, (req, res) => {
   const customerIds = Object.keys(selfServiceWarehouse);
   const sortOrder = req.query.sort;
   if (sortOrder === "asc") {
@@ -19,6 +32,16 @@ app.get("/customers/ids", (req, res) => {
     customerIds.sort((a, b) => b.localeCompare(a));
   }
   res.send(`Customer ids: ${customerIds.join(", ")}.`);
+});
+
+app.get("/customers/:customerId", logRequestInfo, (req, res) => {
+  const customerId = req.params.customerId;
+  const customer = selfServiceWarehouse[customerId];
+  if (customer) {
+    res.send(customer);
+  } else {
+    res.send(`Customer with id ${customerId} not found.`);
+  }
 });
 
 app.listen(port, () => {
