196263d239f25bae1b5444ef19072a5a1601c1b1
Elmerikallio <elmeri.kallio@edu.turkuamk.fi>
2025-10-08 18:07:25 +0300

    vitust tehtävii :-D update readme.md

---
+++ E2T4/server.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("MORO");
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
